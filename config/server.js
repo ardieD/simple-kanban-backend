7ddd398f24f0904a855bcf7c1d2b24cc34f07d08
arethey <ardiederrayal06@gmail.com>
2020-11-19 11:32:18 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea8d37f6cb0edde584d37ca44ad54892'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af997c26-55b3-4737-b898-d95043fd19b4'
+};
